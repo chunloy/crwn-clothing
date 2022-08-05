4c0cad022588b8bdd26bc11ce2b14d32176cc112
Aaron Au <aaron.au@me.com>
2022-08-05 17:53:11 -0600

    add react router as dependency and fix background styling issue

--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -5,7 +5,7 @@ const CategoryItem = (props) => {
 
   return (
     <div className="category-container">
-      <img className="background-img" src={imageURL} alt={title}></img>  
+      <div className="background-img" style={{backgroundImage: `url(${imageURL})`}} />
       <div className="category-body-container">
         <h2>{title}</h2>
         <p>Shop Now</p>
