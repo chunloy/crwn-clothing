d7bf1df0698d286c9b91ce3ee87611f42c259520
Aaron Au <aaron.au@me.com>
2022-08-06 19:14:36 -0600

    Implement navigation component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import Navigation from "./routes/Navigation";
 import Home from "./routes/Home";
 import {Routes, Route} from 'react-router-dom';
 
 const App = () => {
   return(
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+      </Route>
     </Routes>
     
   ); 
