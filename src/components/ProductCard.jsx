0bf856ce4c2516b5d445787300fe412e599da4b8
Aaron Au <aaron.au@me.com>
2022-08-12 17:07:14 -0600

    Create product card component and refactor shop component

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,19 @@
+import Button from "./Button";
+
+import "./ProductCard.scss";
+
+const ProductCard = ({ name, price, imageURL }) => {
+  return (
+    <div className="product-card-container">
+      <img src={imageURL} alt={name} />
+      <div className="footer">
+        <span className="product-name">{name}</span>
+        <span className="product-price">{price}</span>
+      </div>
+      <Button buttonType="inverted">Add to Cart</Button>
+    </div>
+
+  );
+}
+
+export default ProductCard;
--- src/routes/Shop.jsx
+++ src/routes/Shop.jsx
@@ -1,23 +1,13 @@
 import { useContext } from "react";
+import ProductCard from "../components/ProductCard";
 import { ProductsContext } from "../contexts/Products";
 
 const Shop = () => {
   const { products } = useContext(ProductsContext);
 
-  const productsList = products.map(({id, name}) => {
-    return (
-      <div key={id}>
-        <h1>{name}</h1>
-      </div>
-    );
-  });
+  const productsList = products.map(({id, ...productDetails}) => <ProductCard key={id} {...productDetails}/>);
 
-  return (
-    <div>
-      {productsList}
-    </div>
-    
-  );
-};
+  return <div>{productsList}</div>;
+ };
 
 export default Shop;
