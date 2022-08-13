4babc9fa569bd3b160f363775f69a7dd308d10ce
Aaron Au <aaron.au@me.com>
2022-08-12 23:25:34 -0600

    style shop page

--- src/routes/Shop.jsx
+++ src/routes/Shop.jsx
@@ -1,13 +1,14 @@
 import { useContext } from "react";
 import ProductCard from "../components/ProductCard";
 import { ProductsContext } from "../contexts/Products";
+import "./Shop.scss"
 
 const Shop = () => {
   const { products } = useContext(ProductsContext);
 
   const productsList = products.map(({id, ...productDetails}) => <ProductCard key={id} {...productDetails}/>);
 
-  return <div>{productsList}</div>;
+  return <div className="products-container">{productsList}</div>;
  };
 
 export default Shop;
