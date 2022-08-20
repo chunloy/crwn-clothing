290dd891c237289ee3d35b3ae6c6876610c1472e
Aaron Au <aaron.au@me.com>
2022-08-19 23:51:23 -0600

    Fix clashing styles with category components

--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -1,17 +1,20 @@
 import "./CategoryItem.scss";
 
 const CategoryItem = (props) => {
-  const {title, imageURL} = props;
+  const { title, imageURL } = props;
 
   return (
-    <div className="category-container">
-      <div className="background-img" style={{backgroundImage: `url(${imageURL})`}} />
-      <div className="category-body-container">
+    <div className="category-item-container">
+      <div
+        className="background-img"
+        style={{ backgroundImage: `url(${imageURL})` }}
+      />
+      <div className="category-item-body-container">
         <h2>{title}</h2>
         <p>Shop Now</p>
       </div>
     </div>
   );
-}
+};
 
 export default CategoryItem;
