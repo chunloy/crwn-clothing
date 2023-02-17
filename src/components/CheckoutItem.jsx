bd7c41b0263922ecc7e8efb8a03f0e687cbccb50
Aaron Y <101907461+chunloy@users.noreply.github.com>
2023-02-17 00:21:20 -0800

    refactor checkout item component using styled components

--- src/components/CheckoutItem.jsx
+++ src/components/CheckoutItem.jsx
@@ -1,6 +1,15 @@
 import { useContext } from "react";
 import { CartContext } from "../contexts/CartContext";
-import "./CheckoutItem.scss";
+import {
+  CheckoutItemContainer,
+  ImgContainer,
+  Span,
+  Price,
+  Quantity,
+  Value,
+  Arrow,
+  RemoveButton,
+} from "./CheckoutItemStyles";
 
 const CheckoutItem = ({ cartItem }) => {
   const { name, imageURL, quantity, price } = cartItem;
@@ -20,29 +29,21 @@ const CheckoutItem = ({ cartItem }) => {
   };
 
   return (
-    <div className="checkout-item-container">
-      <div className="img-container">
+    <CheckoutItemContainer>
+      <ImgContainer>
         <img src={imageURL} alt={name} />
-      </div>
+      </ImgContainer>
 
-      <span className="name">{name}</span>
-      <span className="quantity">
-        <div className="arrow" onClick={onRemoveHandler}>
-          &#10094;
-        </div>
+      <Span>{name}</Span>
+      <Quantity>
+        <Arrow onClick={onRemoveHandler}>&#10094;</Arrow>
+        <Value>{quantity}</Value>
+        <Arrow onClick={onAddHandler}>&#10095;</Arrow>
+      </Quantity>
 
-        <span className="value">{quantity}</span>
-
-        <div className="arrow" onClick={onAddHandler}>
-          &#10095;
-        </div>
-      </span>
-
-      <span className="price">{price}</span>
-      <div className="remove-button" onClick={onClearHandler}>
-        &#10005;
-      </div>
-    </div>
+      <Price>{price}</Price>
+      <RemoveButton onClick={onClearHandler}>&#10005;</RemoveButton>
+    </CheckoutItemContainer>
   );
 };
 
