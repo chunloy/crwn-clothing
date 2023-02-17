4754506175bc9b581c95d70dbf213f5339e99335
Aaron Y <101907461+chunloy@users.noreply.github.com>
2023-02-16 22:38:01 -0800

    refactor category item component using styled components

--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -1,5 +1,9 @@
 import { useNavigate } from "react-router-dom";
-import "./CategoryItem.scss";
+import {
+  CategoryItemContainer,
+  CategoryItemBodyContainer,
+  BackgroundImage,
+} from "./CategoryItemStyles";
 
 const CategoryItem = ({ category }) => {
   const { title, imageURL, route } = category;
@@ -8,16 +12,13 @@ const CategoryItem = ({ category }) => {
   const onClickHandler = () => navigate(route);
 
   return (
-    <div className="category-item-container" onClick={onClickHandler}>
-      <div
-        className="background-img"
-        style={{ backgroundImage: `url(${imageURL})` }}
-      />
-      <div className="category-item-body-container">
+    <CategoryItemContainer onClick={onClickHandler}>
+      <BackgroundImage imageURL={imageURL} />
+      <CategoryItemBodyContainer>
         <h2>{title}</h2>
         <p>Shop Now</p>
-      </div>
-    </div>
+      </CategoryItemBodyContainer>
+    </CategoryItemContainer>
   );
 };
 
